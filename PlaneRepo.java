0305aeeab3327c71b3e2094f9b58c23a2ecd028b
Issa71 <mohamedissa95@gmail.com>
2022-08-10 10:37:35 +0100

    second commit

--- PlaneRepo.java
+++ PlaneRepo.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
-public interface PlaneRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaneRepo extends JpaRepository<Plane, Integer>{
 
 }
