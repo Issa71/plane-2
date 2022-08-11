dfac723e6ccf7264223e0ead96701e121b5ac833
Issa71 <109733928+Issa71@users.noreply.github.com>
2022-08-11 16:59:47 +0100

    third commit

--- Plane.java
+++ Plane.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Plane {
 	
 	@Id
@@ -34,6 +36,7 @@ public class Plane {
 	public Plane() {
 		super();
 	}
+	
 	public Integer getId() {
 		return this.id;
 	}
